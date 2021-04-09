833c7ced88ad1287b926c8531fa498c0039c8db5
MrinmoiHossain <mokit.aust@gmail.com>
2021-04-10 00:17:12 +0600

    change format

--- jdk/recipes/default.rb
+++ jdk/recipes/default.rb
@@ -1,12 +1,14 @@
-package 'wget'
+# Download Java tar file
+remote_file "/tmp/#{node['java']['install']['file']}" do
+    source "#{node['java']['install']['repo']}/#{node['java']['install']['file']}"
+    owner 'root'
+    group 'root'
+end
 
-bash 'install_java' do
-    code <<-EOH
-        wget "#{node['java']['install']['repo']}/#{node['java']['install']['file']}" -O "/tmp/#{node['java']['install']['file']}"
-	mkdir -p /usr/lib/jvm
-	sudo tar xfvz "/tmp/#{node['java']['install']['file']}" --directory /usr/lib/jvm
-        export PATH=$PATH:/usr/lib/jvm/jdk-11.0.1/bin
-        export JAVA_HOME=/usr/lib/jvm/jdk-11.0.1/
-        export J2SDKDIR=/usr/lib/jvm/jdk-11.0.1/
-    EOH
+# Extract the file
+archive_file '/tmp/#{node['java']['install']['file']}' do
+    path '/tmp/#{node['java']['install']['file']}'
+    destination '/usr/lib/jvm'
+    owner 'root'
+    group 'root'
 end
