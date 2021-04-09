0015c6dff0fe35d3a286c364e5780a9feea3fdca
MrinmoiHossain <mokit.aust@gmail.com>
2021-04-09 21:08:11 +0600

    temp

--- jdk/recipes/default.rb
+++ jdk/recipes/default.rb
@@ -1,8 +1,12 @@
 bash 'install-java' do
     code <<-EOH
+    echo "####################### Mrinmoi ########################\n"
+    echo "{node['java']['install']['repo']}"
     wget "{node['java']['install']['repo']}/{node['java']['install']['file']}" -O "/tmp/{node['java']['install']['file']}"
-    sudo tar xfvz "/tmp/{node['java']['install']['file']}" --directory /usr/lib/jvm
-    unlink java
-    ln -s /usr/lib/jvm/jdk-11 java
+    pwd
+    
+    #sudo tar xfvz "/tmp/{node['java']['install']['file']}" --directory /usr/lib/jvm
+    #unlink java
+    #ln -s /usr/lib/jvm/jdk-11 java
     EOH
 end
