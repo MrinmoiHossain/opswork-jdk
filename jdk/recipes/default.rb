99edf6df15974f15901e70a255bfaf70dabbe05a
MrinmoiHossain <mokit.aust@gmail.com>
2021-04-09 22:46:19 +0600

    untar

--- jdk/recipes/default.rb
+++ jdk/recipes/default.rb
@@ -9,5 +9,6 @@ end
 bash 'install_java' do
     code <<-EOH
         wget "#{node['java']['install']['repo']}/#{node['java']['install']['file']}" -O "/tmp/#{node['java']['install']['file']}"
+	sudo tar xfvz "/tmp/#{node['java']['install']['file']}" --directory /usr/lib/jvm
     EOH
 end
