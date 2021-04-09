8c9cae83d2b357efac556bca454911e462991682
MrinmoiHossain <mokit.aust@gmail.com>
2021-04-09 21:17:45 +0600

    wget using link

--- jdk/recipes/default.rb
+++ jdk/recipes/default.rb
@@ -2,7 +2,8 @@ bash 'install-java' do
     code <<-EOH
     echo "####################### Mrinmoi ########################\n"
     echo "{node['java']['install']['repo']}"
-    wget "{node['java']['install']['repo']}/{node['java']['install']['file']}" -O "/tmp/{node['java']['install']['file']}"
+    #wget "{node['java']['install']['repo']}/{node['java']['install']['file']}" -O "/tmp/{node['java']['install']['file']}"
+    wget https://download.java.net/java/GA/jdk11/28/GPL/openjdk-11+28_linux-x64_bin.tar.gz -O /tmp/openjdk-11+28_linux-x64_bin.tar.gz
     pwd
     
     #sudo tar xfvz "/tmp/{node['java']['install']['file']}" --directory /usr/lib/jvm
