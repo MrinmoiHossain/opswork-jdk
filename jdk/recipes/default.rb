0591ca12a5d7f6c2e3bac57dbe76eeefa48a1bc9
MrinmoiHossain <mokit.aust@gmail.com>
2021-04-09 23:31:42 +0600

    java path

--- jdk/recipes/default.rb
+++ jdk/recipes/default.rb
@@ -11,5 +11,8 @@ bash 'install_java' do
         wget "#{node['java']['install']['repo']}/#{node['java']['install']['file']}" -O "/tmp/#{node['java']['install']['file']}"
 	mkdir -p /usr/lib/jvm
 	sudo tar xfvz "/tmp/#{node['java']['install']['file']}" --directory /usr/lib/jvm
+        export PATH=$PATH:/usr/lib/jvm/jdk-11.0.1/bin
+        export JAVA_HOME=/usr/lib/jvm/jdk-11.0.1/
+        export J2SDKDIR=/usr/lib/jvm/jdk-11.0.1/
     EOH
 end
