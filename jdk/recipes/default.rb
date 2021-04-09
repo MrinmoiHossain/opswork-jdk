c961fda1f7324e155546988f6c806dee5f533880
MrinmoiHossain <mokit.aust@gmail.com>
2021-04-10 01:45:21 +0600

    create dir

--- jdk/recipes/default.rb
+++ jdk/recipes/default.rb
@@ -5,6 +5,12 @@ remote_file "/tmp/#{node['java']['install']['file']}" do
     group 'root'
 end
 
+bash 'make_dir' do
+    code <<-EOH
+        mkdir -p /usr/lib/jvm
+    EOH
+end
+
 # Extract the file
 tar_extract "/tmp/#{node['java']['install']['file']}" do
     action :extract_local
