d804bda2e2b7dcec39c2404a2c88680d92ef81ce
MrinmoiHossain <mokit.aust@gmail.com>
2021-04-09 21:29:21 +0600

    test

--- jdk/recipes/default.rb
+++ jdk/recipes/default.rb
@@ -1,9 +1,16 @@
-bash 'install-java' do
+package 'wget'
+
+bash 'make_dir' do
     code <<-EOH
-    echo "####################### Mrinmoi ########################\n"
-    echo "{node['java']['install']['repo']}"
+        mkdir -p "~/hello"
+        mkdir -p "~/{node['java']['install']['repo']}"
+    EOH
+end
+
+bash 'install_java' do
+    code <<-EOH
     #wget "{node['java']['install']['repo']}/{node['java']['install']['file']}" -O "/tmp/{node['java']['install']['file']}"
-    wget https://download.java.net/java/GA/jdk11/28/GPL/openjdk-11+28_linux-x64_bin.tar.gz -O /tmp/openjdk-11+28_linux-x64_bin.tar.gz
+    #wget https://download.java.net/java/GA/jdk11/28/GPL/openjdk-11+28_linux-x64_bin.tar.gz -O /tmp/openjdk-11+28_linux-x64_bin.tar.gz
     pwd
     
     #sudo tar xfvz "/tmp/{node['java']['install']['file']}" --directory /usr/lib/jvm
