5d539a47ec5ba662f0527ba6de1e0847f7517770
MrinmoiHossain <mokit.aust@gmail.com>
2021-04-10 20:52:47 +0600

    bugs

--- jdk/recipes/default.rb
+++ jdk/recipes/default.rb
@@ -15,7 +15,7 @@ end
 # Extract the file
 bash 'make_dir' do
     code <<-EOH
-        sudo tar xfvz "/tmp/#{node['java']['install']['file']}" --directory #{node['java']['jvm_dir']}
+        sudo tar xfvz "/tmp/#{node['java']['install']['file']}" --directory "#{node['java']['jvm_dir']}"
     EOH
 end
 
