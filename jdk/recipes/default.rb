2946f98b848cd881da40e1e4a2133dda6ec5a04a
MrinmoiHossain <mokit.aust@gmail.com>
2021-04-10 11:31:20 +0600

    java home

--- jdk/recipes/default.rb
+++ jdk/recipes/default.rb
@@ -5,7 +5,7 @@ remote_file "/tmp/#{node['java']['install']['file']}" do
     group 'root'
 end
 
-# Make Directory
+# Make Directory and Extract the file
 bash 'make_dir' do
     code <<-EOH
 	mkdir -p /usr/lib/jvm
@@ -13,9 +13,7 @@ bash 'make_dir' do
     EOH
 end
 
-# Extract the file
-
-
+# Set Java Home
 directory '/etc/profile.d' do
     mode '0755'
 end
@@ -25,4 +23,20 @@ template '/etc/profile.d/jdk.sh' do
     mode '0755'
 end
 
+if node['java']['set_etc_environment'] # ~FC023 -- Fails unit test to use guard
+    ruby_block 'Set JAVA_HOME in /etc/environment' do
+        block do
+            file = Chef::Util::FileEdit.new('/etc/environment')
+            file.insert_line_if_no_match(/^PATH=/, "PATH=\$PATH\:#{node['java']['java_home']}/bin")
+            file.insert_line_if_no_match(/^JAVA_HOME=/, "JAVA_HOME=#{node['java']['java_home']}")
+            file.search_file_replace_line(/^JAVA_HOME=/, "JAVA_HOME=#{node['java']['java_home']}")
+            file.write_file
+        end
+    end
+end
 
+# Java Symlink
+link "/usr/lib/jvm/jdk-11.0.1/" do
+    to node['java']['java_home']
+    not_if { node['java']['java_home'] == '/usr/lib/jvm/jdk-11.0.1/' }
+end
