e62e3cf3b0edd222970b46a403bd5fd709f5fd24
MrinmoiHossain <mokit.aust@gmail.com>
2021-04-10 20:32:45 +0600

    delete dir

--- jdk/recipes/create_dir.rb
+++ jdk/recipes/create_dir.rb
@@ -1,3 +1,8 @@
+directory "/etc/Mrinmoi" do
+    action :delete
+    only_if { ::Dir.exist?("/etc/Mrinmoi") || !::File.symlink?("/etc/Mrinmoi") }
+end
+
 directory '/etc/Mrinmoi' do
     owner 'root'
     group 'root'
