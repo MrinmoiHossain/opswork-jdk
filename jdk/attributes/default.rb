c9e978bae617af1aee24eacfc2264729af124a8d
MrinmoiHossain <mokit.aust@gmail.com>
2021-04-10 10:23:09 +0600

    java uri

--- jdk/attributes/default.rb
+++ jdk/attributes/default.rb
@@ -1,6 +1,6 @@
 # Default file
 
-default['java']['install']['repo'] = "https://mirrors.edge.kernel.org/pub/software/scm/git"
-default['java']['install']['file'] = "git-1.8.4.3.tar.gz"
+default['java']['install']['repo'] = "https://download.java.net/java/GA/jdk11/13/GPL"
+default['java']['install']['file'] = "openjdk-11.0.1_linux-x64_bin.tar.gz"
 
 
